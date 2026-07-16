26b56c8f07d67796e4c4fae2a08546007dbf6e02
NewUsername <pebbles_boon@proton.me>
2026-07-16 20:08:46 +1000

    unused path removal

--- src/client/java/com/pebbles_boon/metalrender/performance/MetalRenderProfiler.java
+++ src/client/java/com/pebbles_boon/metalrender/performance/MetalRenderProfiler.java
@@ -7,7 +7,6 @@ import com.pebbles_boon.metalrender.util.MetalLogger;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
