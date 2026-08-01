4854a02cd1d28ac4f50eadba39313e9010c00a8b
NewUsername <pebbles_boon@proton.me>
2026-08-01 19:54:25 +1000

    added compile commands

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 rm -f run/logs/latest.log
+./compile_native.sh
 
 # Use Java 25 for Fabric Loom
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-25.jdk/Contents/Home && ./gradlew runClient
