8b3856dd3428d29859735e860e795c503a7b395d
webblepebbles <pebbles_boon@proton.me>
2026-08-16 20:22:59 +1000

    mixin target fix

--- src/client/java/com/pebbles_boon/metalrender/sodium/mixins/SpriteContentsTickerMixin.java
+++ src/client/java/com/pebbles_boon/metalrender/sodium/mixins/SpriteContentsTickerMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net.minecraft.client.texture.SpriteContents$Animator")
+@Mixin(targets = "net.minecraft.client.renderer.texture.SpriteContents$AnimationState")
 public class SpriteContentsTickerMixin {
 
   @Inject(method = "tick", at = @At("HEAD"))
