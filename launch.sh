c21b773e6a1214fef56a225371085629722bd5f2
webblepebbles <pebbles_boon@proton.me>
2026-09-06 18:28:58 +1000

    changed bc i switched

--- launch.sh
+++ launch.sh
@@ -3,6 +3,4 @@ cd "$(dirname "$0")"
 rm -f run/logs/latest.log
 ./compile_native.sh
 
-# Use Java 25 for Fabric Loom
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-25.jdk/Contents/Home && ./gradlew runClient
-
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-25.jdk/Contents/Home && ./gradlew runClient
